b6fa57a3ec307fb1a7b67fb3bd54bad867d1d7d2
akmalzaki <akmalzaki311203@gmail.com>
2024-05-11 23:54:27 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "inter" : ['inter']
+      },
+      colors:{
+        "grey" :'#667085',
+      }
+    },
+  },
+  plugins: [],
+}
+
